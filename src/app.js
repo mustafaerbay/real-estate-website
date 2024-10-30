9887d62030332ed05e088638b894ab7ef24bd109
Mustafa Erbay <mustafaerbay365@gmail.com>
2024-10-30 07:37:50 -0400

    move products to product.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-export const company_details = {
+import { products, categories } from './product';
+const company_details = {
     "tax_number": "721 097 3955",
     "company_full_name": "PARADOKS ENERJI VE GAYRIMENKUL TIC. LTD. STI. ",
     "address_direction": "https://maps.app.goo.gl/6gqvFzKakxfT1oRXA",
@@ -18,13 +19,6 @@ export const company_details = {
         { instagram: '/#' },
         { twitter: '/#' }
     ]
-}
-const contact_info = {
-    "name": "",
-    "surname": "",
-    "email": "",
-    "no": "",
-    "detail": ""
 }
 const links = [
     {
@@ -48,84 +42,5 @@ const links = [
         "is_active": false
     },
 ]
-const categories = [
-    {
-        "id": 1,
-        "name": "HOME FOR SALE",
-        "short_name": "HOME",
-        "description": "Latest gadgets and electronics",
-        "url": "images/foto-01.webp"
-    },
-    {
-        "id": 2,
-        "name": "OFFICE RENTALS",
-        "short_name": "OFFICE",
-        "description": "Appliances for your home",
-        "url": "images/foto-02.webp"
-    },
-    {
-        "id": 3,
-        "name": "BUY LANDS",
-        "short_name": "LAND",
-        "description": "ARSA ALIM-SATIM ISLERI",
-        "url": "images/foto-03.webp"
-    },
-    {
-        "id": 4,
-        "name": "RENTALS",
-        "short_name": "RENTALS",
-        "description": "Wide range of books and literature",
-        "url": "images/foto-04.webp"
-    }
-]
-const products = [
-    {
-        "id": 101,
-        "is_highlighted": true,
-        "name": "demo 01",
-        "category_id": 1,
-        "price": 799.99,
-        "stock": 25,
-        "description": "demo 01 aciklama",
-        "image_url": "images/foto-01.webp",
-        "tags": ["365m2", "5.kat"],
-        "long_description": "demo 01 uzun aciklama"
-    },
-    {
-        "id": 201,
-        "is_highlighted": false,
-        "name": "demo 02",
-        "category_id": 2,
-        "price": 99.99,
-        "stock": 25,
-        "description": "demo 02 aciklama",
-        "image_url": "images/foto-02.webp",
-        "tags": ["350m2", "1.kat"],
-        "long_description": "demo 02 uzun aciklama"
-    },
-    {
-        "id": 301,
-        "is_highlighted": false,
-        "name": "demo 03",
-        "category_id": 3,
-        "price": 99.99,
-        "stock": 25,
-        "description": "demo 03 aciklama",
-        "image_url": "images/foto-03.webp",
-        "tags": ["1000m2", "10.kat"],
-        "long_description": "demo 03 uzun aciklama"
-    },
-    {
-        "id": 401,
-        "is_highlighted": false,
-        "name": "demo 04",
-        "category_id": 4,
-        "price": 99.99,
-        "stock": 25,
-        "description": "demo 04 aciklama",
-        "image_url": "images/foto-04.webp",
-        "tags": ["123m2", "29.kat"],
-        "long_description": "demo 04 uzun aciklama"
-    }
-]
-export { phone, email, whatsapp, workHours, mapUrl, address, categories, products, links }
+
+export { company_details, categories, products, links }
---
+++ src/product.js
@@ -0,0 +1,82 @@
+export const products = [
+    {
+        "id": "101",
+        "is_highlighted": false,
+        "name": "demo 01",
+        "category_id": 1,
+        "price": 799.99,
+        "stock": 25,
+        "description": "demo 01 aciklama",
+        "image_url": "images/foto-01.webp",
+        "tags": ["365m2", "5.kat"],
+        "long_description": "demo 01 uzun aciklama"
+    },
+    {
+        "id": "201",
+        "is_highlighted": true,
+        "name": "Astoria Full",
+        "category_id": 2,
+        "price": 99.99,
+        "stock": 25,
+        "description": "demo 02 aciklama",
+        "image_url": "images/foto-02.webp",
+        "image_list": ["images/listings/astoria"],
+        "tags": ["350m2", "1.kat"],
+        "long_description": "demo 02 uzun aciklama"
+    },
+    {
+        "id": "301",
+        "is_highlighted": false,
+        "name": "demo 03",
+        "category_id": 3,
+        "price": 99.99,
+        "stock": 25,
+        "description": "demo 03 aciklama",
+        "image_url": "images/foto-03.webp",
+        "tags": ["1000m2", "10.kat"],
+        "long_description": "demo 03 uzun aciklama"
+    },
+    {
+        "id": "401",
+        "is_highlighted": false,
+        "name": "demo 04",
+        "category_id": 4,
+        "price": 99.99,
+        "stock": 25,
+        "description": "demo 04 aciklama",
+        "image_url": "images/foto-04.webp",
+        "tags": ["123m2", "29.kat"],
+        "long_description": "demo 04 uzun aciklama"
+    }
+]
+
+export const categories = [
+    {
+        "id": 1,
+        "name": "HOME FOR SALE",
+        "short_name": "HOME",
+        "description": "Latest gadgets and electronics",
+        "url": "images/foto-01.webp"
+    },
+    {
+        "id": 2,
+        "name": "OFFICE RENTALS",
+        "short_name": "OFFICE",
+        "description": "Appliances for your home",
+        "url": "images/foto-02.webp"
+    },
+    {
+        "id": 3,
+        "name": "BUY LANDS",
+        "short_name": "LAND",
+        "description": "ARSA ALIM-SATIM ISLERI",
+        "url": "images/foto-03.webp"
+    },
+    {
+        "id": 4,
+        "name": "RENTALS",
+        "short_name": "RENTALS",
+        "description": "Wide range of books and literature",
+        "url": "images/foto-04.webp"
+    }
+]
