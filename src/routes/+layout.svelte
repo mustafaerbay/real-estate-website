85fc8a2ab07f07c30d19ec7d9d17bac3f1b0b6ee
Mustafa Erbay <mustafaerbay365@gmail.com>
2025-11-15 10:36:17 +0300

    add tracking.js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -70,7 +70,7 @@
     </script>
     <script src="https://metricfortune.vercel.app/tracking.js"></script>
     <script>
-        MetricFortune.init({ siteId: "site_p4NITpD3mnhRTbI0" });
+        MetricFortune.init({ siteId: "site_zGTEgkgBWGLXriJa" });
     </script>
     <!-- <script>
         (function () {
